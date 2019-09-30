acdf675e4dabca0bba92e209ae74f4b777b904aa
liciolentimo <lentimo@gmail.com>
2019-09-30 12:00:41 +0300

    updated content

---
+++ main.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDIHNKFbkckxPLmL9MWkcJJ_4bvYNQLqaM",
+    authDomain: "angularlogin-eb60a.firebaseapp.com",
+    databaseURL: "https://angularlogin-eb60a.firebaseio.com",
+    projectId: "angularlogin-eb60a",
+    storageBucket: "angularlogin-eb60a.appspot.com",
+    messagingSenderId: "240474975765",
+    appId: "1:240474975765:web:6a01f90255891b50fc281c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
